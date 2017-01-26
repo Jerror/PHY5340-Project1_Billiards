28dad52132eedd32c0e30ddb8140a71df5040c3f
Jeremiah O'Neil <jeremiah.d.oneil@gmail.com>
2017-01-26 11:44:27 -0500

    alpha version unit circle boundary and billiard propagation routine.

--- billiards.py
+++ billiards.py
@@ -1,3 +1,19 @@
 import numpy as np
+from boundary import *
 
+bdy = UnitCircleBoundary()
+x0 = np.array([0, 0])
+vel = np.array([3.0/5, 4.0/5])
+s0 = bdy.linear_intersect_cart(x0, vel)
+if np.any(np.sign(bdy.coords_cart(s0) - x0) != np.sign(vel)):
+    # We found the intercept in the wrong direction! Try again.
+    s0 = bdy.linear_intersect_param(s0, vel)
+collisions = [s0]
+for i in range(10):
+    tangent = bdy.tangent_cart(collisions[-1])
+    vel = 2*np.dot(vel, tangent) * tangent/np.linalg.norm(tangent)**2 - vel
+    print vel
+    collisions += [bdy.linear_intersect_param(collisions[-1], vel)]
+
+print collisions
 
--- boundary.py
+++ boundary.py
@@ -1,6 +1,7 @@
 """Classes of 1D convex boundaries providing methods for billiard sims."""
 import abc
 import numpy as np
+import scipy.optimize as opt
 
 class BilliardBoundary_abstract(object):
     """Define 1D convex boundary methods required for billiard sims."""
@@ -13,16 +14,31 @@ class BilliardBoundary_abstract(object):
         raise NotImplementedError('')
 
     @abc.abstractmethod
-    def linear_intersect(self, start, direction):
-        """Get (the parameter value) at the intersection with given line.
-        return s"""
+    def tangent_cart(self, s):
+        """Get the cartesian tangent vector at parameter value s.
+        return np.array([x, y])"""
         raise NotImplementedError('')
 
     @abc.abstractmethod
-    def tangent_unit_cart(self, s):
-        """Get the normalized cartesian tangent vector at parameter value s.
-        return np.array([x, y])"""
+    def linear_intersect_cart(self, x0, v):
+        """Get (the parameter value) at the intersection with given line.
+        This variant expects cartesian start x0."""
         raise NotImplementedError('')
+
+    @abc.abstractmethod
+    def linear_intersect_param(self, s0, v):
+        """Get (the parameter value) at the intersection with given line.
+        This variant expects parametric start s0."""
+        raise NotImplementedError('')
+        
+    def _linear_intersect_function(self, x0, v):
+        """Return function whose root gives bdy intersect with given line."""
+        return lambda s: np.dot(self.coords_cart(s) - x0, 
+                                np.array([v[1], -v[0]]))
+
+    def _linear_inter_func_derivative(self, v):
+        return lambda s: np.dot(self.tangent_cart(s), np.array([v[1], -v[0]]))
+
 
 class UnitCircleBoundary(BilliardBoundary_abstract):
     """Circular boundary of unit radius parameterized by angle."""
@@ -30,5 +46,22 @@ class UnitCircleBoundary(BilliardBoundary_abstract):
     def coords_cart(self, s):
         return np.array([np.cos(s), np.sin(s)])
 
-    def linear_intersect(self, start, direction):
-        
+    def tangent_cart(self, s):
+        return np.array([-np.sin(s), np.cos(s)])
+
+    def _linear_inter_func_d2(self, v):
+        return lambda s:np.dot(-self.coords_cart(s), np.array([v[1], -v[0]]))
+
+    def linear_intersect_cart(self, x0, v):
+        """Using Halley's parabolic root finder"""
+        return opt.newton(self._linear_intersect_function(x0, v), np.pi,
+                          fprime=self._linear_inter_func_derivative(v),
+                          fprime2=self._linear_inter_func_d2(v))
+
+    def linear_intersect_param(self, s0, v):
+        """Using Brent's method with quadratic interpolation.
+        This method searches in a specified interval, so I can exclude s0."""
+        x0 = self.coords_cart(s0)
+        return opt.brentq(self._linear_intersect_function(x0, v),
+                          (s0 + 0.01) % (2*np.pi), (s0 - 0.01) % (2*np.pi))
+
