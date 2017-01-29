162611b79b83266a05153000e781be285b97e665
Jeremiah O'Neil <jeremiah.d.oneil@gmail.com>
2017-01-29 18:52:07 -0500

    adds ContinuousDifferentiableBoundary metaclass, which abstracts the rootfinding methods of UnitCircleBoundary (now a child class)

--- billiards.py
+++ billiards.py
@@ -3,14 +3,15 @@ import matplotlib.pyplot as plt
 from boundary import *
 
 bdy = UnitCircleBoundary()
-x0 = np.array([0.7, 0.2])
-vel = np.array([3.0/5, 4.0/5])
+x0 = np.array([0.6, 0.2])
+theta = 0.1
+vel = np.array([np.cos(theta), np.sin(theta)])
 s0 = bdy.linear_intersect_cart(x0, vel)
 if np.any(np.sign(bdy.coords_cart(s0) - x0) != np.sign(vel)):
-    # We found the intercept in the wrong direction! Try again.
-    s0 = bdy.linear_intersect_param(s0, vel)
+    # Then we found the intercept in the wrong direction! Try again.
+    s0 = bdy.linear_intersect_param(s0, vel)  # Excludes passed root s0
 collisions = [s0]
-for i in range(10):
+for i in range(50):
     tangent = bdy.tangent_cart(collisions[-1])
     vel = 2*np.dot(vel, tangent) * tangent/np.linalg.norm(tangent)**2 - vel
     collisions += [bdy.linear_intersect_param(collisions[-1], vel)]
--- boundary.py
+++ boundary.py
@@ -9,7 +9,7 @@ class BilliardBoundary_abstract(object):
 
     @abc.abstractmethod
     def coords_cart(self, s):
-        """Get the cartesian coordinates at parameter value s.        
+        """Get the cartesian coordinates at parameter value s.
         return np.array([x, y])"""
         raise NotImplementedError('')
 
@@ -30,27 +30,29 @@ class BilliardBoundary_abstract(object):
         """Get (the parameter value) at the intersection with given line.
         This variant expects parametric start s0."""
         raise NotImplementedError('')
-        
+
     def _linear_intersect_function(self, x0, v):
         """Return function whose root gives bdy intersect with given line."""
-        return lambda s: np.dot(self.coords_cart(s) - x0, 
+        return lambda s: np.dot(self.coords_cart(s) - x0,
                                 np.array([v[1], -v[0]]))
 
     def _linear_inter_func_derivative(self, v):
         return lambda s: np.dot(self.tangent_cart(s), np.array([v[1], -v[0]]))
 
 
-class UnitCircleBoundary(BilliardBoundary_abstract):
-    """Circular boundary of unit radius parameterized by angle."""
-    
-    def coords_cart(self, s):
-        return np.array([np.cos(s), np.sin(s)])
+class ContinuousDifferentiableBoundary_abstract(BilliardBoundary_abstract):
+    """Billiard methods for 1D continuous differentiable convex boundaries."""
+    __metaclass__ = abc.ABCMeta
 
-    def tangent_cart(self, s):
-        return np.array([-np.sin(s), np.cos(s)])
+    def __init__(self, domain):
+        # domain is the period of the parameter (assumed cyclic, 0 origin)
+        self.domain = domain
 
+    @abc.abstractmethod
     def _linear_inter_func_d2(self, v):
-        return lambda s:np.dot(-self.coords_cart(s), np.array([v[1], -v[0]]))
+        """Second derivative of linear intersection function, for second-order
+        rootfinding methods."""
+        raise NotImplementedError('')
 
     def linear_intersect_cart(self, x0, v):
         """Using Halley's parabolic root finder"""
@@ -62,6 +64,23 @@ class UnitCircleBoundary(BilliardBoundary_abstract):
         """Using Brent's method with quadratic interpolation.
         This method searches in a specified interval, so I can exclude s0."""
         x0 = self.coords_cart(s0)
-        return opt.brentq(self._linear_intersect_function(x0, v),
-                          s0 + 0.01, s0 + 2*np.pi - 0.01) % (2*np.pi)
+        s = opt.brentq(self._linear_intersect_function(x0, v),
+                       s0 + 0.001*self.domain, s0 + 0.999*self.domain)
+        return s % self.domain
+
+
+class UnitCircleBoundary(ContinuousDifferentiableBoundary_abstract):
+    """Circular boundary of unit radius parameterized by angle."""
+
+    def __init__(self):
+        super(UnitCircleBoundary, self).__init__(2*np.pi)
+
+    def coords_cart(self, s):
+        return np.array([np.cos(s), np.sin(s)])
+
+    def tangent_cart(self, s):
+        return np.array([-np.sin(s), np.cos(s)])
+
+    def _linear_inter_func_d2(self, v):
+        return lambda s:np.dot(-self.coords_cart(s), np.array([v[1], -v[0]]))
 
