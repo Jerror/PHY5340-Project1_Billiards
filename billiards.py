d2baec3945154b96ebb58c01365e83f81409f3ea
Jeremiah O'Neil <jeremiah.d.oneil@gmail.com>
2017-02-07 16:46:05 -0500

    refactor to separate plot interactivity and billiard propagation

--- billiards.py
+++ billiards.py
@@ -33,10 +33,24 @@ def billiard_propagator_gen(r, v, bdy):
         yield s
 
 
+def init_state(r, v, boundary):
+    state = {'collisions': [],
+             'trajectory': np.c_[r],
+             'propagator': billiard_propagator_gen(r, v, bdy)}
+    return state
+
+
+def propagate(state, bounces):
+    new_collisions = [state['propagator'].next() for _ in xrange(bounces)]
+    state['collisions'].append(new_collisions)
+    state['trajectory'] = np.c_[state['trajectory'],
+                                bdy.coords_cart(np.array(new_collisions))]
+
+
 bdyline = bdy.coords_cart(np.arange(0, 2*np.pi, 0.01))
 plt.plot(bdyline[0], bdyline[1])
 # Plot a line object "l"; I'll send data to it later 
-l, = plt.plot([0, 0], [0, 0], label='Trajectory')  
+traj_line, = plt.plot([0, 0], [0, 0], label='Trajectory')  
 plt.axis('equal')
 
 # Make Sliders (with their own axis artists) for interactive control.
@@ -45,45 +59,36 @@ axbounces = plt.axes([0.2, 0.03, 0.65, 0.02], axisbg=axcolor)
 sbounces = Slider(axbounces, 'Number of bounces',
                   1, max_bounces, valinit=default_bounces)
 axdeltay = plt.axes([0.2, 0.005, 0.65, 0.02], axisbg=axcolor)
-sdeltay = Slider(axdeltay, r'$\Delta y_0$ / 10^{-8} $y_0$',
+sdeltay = Slider(axdeltay, r'$\Delta y_0$ / $(y_0 * 10^{-8})$',
                  -max_deltay, max_deltay, valinit=default_deltay)
 
-state = {'collisions': [],
-         'trajectory': [],
-         'propagator': iter([])}
 
-
-def update_trajectory(state):
-    n = int(sbounces.val)
-#    print state['trajectory']
+def propagate_plotline(state, n, plotline):
     missing = n+1 - state['trajectory'].shape[1]
     if missing <= 0:
-        l.set_xdata(state['trajectory'][0, 0:n+1])
-        l.set_ydata(state['trajectory'][1, 0:n+1])
+        plotline.set_xdata(state['trajectory'][0, 0:n+1])
+        plotline.set_ydata(state['trajectory'][1, 0:n+1])
     else:
-        new_collisions = [state['propagator'].next() for _ in xrange(missing)]
-        state['collisions'].append(new_collisions)
-        state['trajectory'] = np.c_[state['trajectory'],
-                                    bdy.coords_cart(np.array(new_collisions))]
-        l.set_xdata(state['trajectory'][0])
-        l.set_ydata(state['trajectory'][1])
-#        print n, missing, state['trajectory']
+        propagate(state, missing)    
+        plotline.set_xdata(state['trajectory'][0])
+        plotline.set_ydata(state['trajectory'][1])
     plt.draw()
 
 
-def update_ic(state):
-    rinit = r0 * np.array([1, 1 + sdeltay.val*1e-8])
-    state['trajectory'] = np.c_[rinit]
-    state['propagator'] = billiard_propagator_gen(rinit, v0, bdy)
-    update_trajectory(state)
+def reset_plotline(r, v, boundary, n, plotline):
+    state = init_state(r, v, boundary)
+    propagate_plotline(state, n, plotline)
+    return state
 
 # Initialize plotline data for default Slider settings.
-update_ic(state)
+state = reset_plotline(r0 * np.array([1, 1 + sdeltay.val*1e-8]), v0, bdy,
+                       int(sbounces.val), traj_line)
 # Configure our Sliders to update the plotlines.
-# The Sliders will unnecessarily pass their current value to the on_changed
-#  event callback; I consume this value with a lambda wrapper.
-sbounces.on_changed(lambda val: update_trajectory(state))
-sdeltay.on_changed(lambda val: update_ic(state))
+# The Sliders will pass their current value to the on_changed event callback
+sbounces.on_changed(lambda val: propagate_plotline(state, int(val), traj_line))
+sdeltay.on_changed(lambda val: reset_plotline(r0 * np.array([1, 1 + val*1e-8]),
+                                              v0, bdy, int(sbounces.val),
+                                              traj_line))
 
 plt.show()
 
